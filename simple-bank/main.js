8c7898cd8ced3a146ad2f471c0762384c66a587a
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-30 15:57:22 +0600

    deposit complete

--- simple-bank/main.js
+++ simple-bank/main.js
@@ -1,6 +1,23 @@
-document.getElementById("login").addEventListener('click',function () {
+document.getElementById("login").addEventListener('click', function () {
     const loginArea = document.getElementById("login-area")
     const transactionArea = document.getElementById("transaction-area")
-    loginArea.style.display='none';
-    transactionArea.style.display='block'
+    loginArea.style.display = 'none';
+    transactionArea.style.display = 'block'
+})
+
+// Deposit area
+
+const depositBtn = document.getElementById('addDeposit')
+
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const currentDeposit = document.getElementById('currentDeposit')
+    const currentBalance = document.getElementById('currentBalance');
+
+    const totalDeposit = parseInt(depositAmount) + parseInt(currentDeposit.innerText);
+    const currentTotalBalance = parseInt(currentBalance.innerText) + parseInt(depositAmount);
+
+    currentDeposit.innerText = totalDeposit;
+    currentBalance.innerText = currentTotalBalance;
+
 })
