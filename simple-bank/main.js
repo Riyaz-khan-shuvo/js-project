b82b3f081fd9bb8ce2bf99109ae3518453646632
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-30 15:44:57 +0600

    transaction area show done

---
+++ simple-bank/main.js
@@ -0,0 +1,6 @@
+document.getElementById("login").addEventListener('click',function () {
+    const loginArea = document.getElementById("login-area")
+    const transactionArea = document.getElementById("transaction-area")
+    loginArea.style.display='none';
+    transactionArea.style.display='block'
+})
